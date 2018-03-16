1eb737265741a7f343435db2340e6c680327dbd2
Wazhabits <anatole.piveteau@gmail.com>
2018-03-16 09:34:33 +0100

    Fixed bug : Event Update

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -315,9 +315,9 @@
                                                 <span style="width: 150px; overflow: hidden; text-align: left;">{{$e->title}}</span>
                                                 <form action="{{action('IndexController@SendInvitationVote')}}" method="post" style="width: 100px;">{{csrf_field()}}<input type="hidden" name="id" value="{{$e->id}}"><button class="btn btn-primary btn" style="width:100px;">Inviter <i class="far fa-envelope"></i></button></form>
                                                 <form action="{{action('IndexController@CloseEvent')}}" method="post">{{csrf_field()}}<input type="hidden" name="id" value="{{$e->id}}"><button class="btn btn-danger btn" style="width:100px;">Cloturer <i class="fas fa-times"></i></button></form>
-                                                <a href="#AddPart" data-toggle="collapse" style="display: inline-block; width: 50px; height: 50px; font-size: 30px;"><i class="far fa-edit"></i></a>
+                                                <a href="#AddPart{{$e->id}}" data-toggle="collapse" style="display: inline-block; width: 50px; height: 50px; font-size: 30px;"><i class="far fa-edit"></i></a>
                                             </h5>
-                                            <div class="collapse" id="AddPart">
+                                            <div class="collapse" id="AddPart{{$e->id}}">
                                                 <form action="{{action('IndexController@ParticipantUpdater')}}" method="post">
                                                     {{csrf_field()}}
                                                     <input type="hidden" name="id" value="{{$e->id}}">
