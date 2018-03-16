07e5badb76d2036fbeb590ca73bbe64eef75a9b3
Wazhabits <anatole.piveteau@gmail.com>
2018-03-16 09:28:16 +0100

    Fixed bug : new line

--- app/Events.php
+++ app/Events.php
@@ -30,7 +30,10 @@ class Events extends Model
             $i = 0;
             while ($i < count($answer)) {
                 $nAnswer = new Answers();
-                $nAnswer->answer = $answer[$i];
+                $mail = str_replace("\r", "", $answer[$i]);
+                $mail = str_replace('\n', "", $mail);
+                $mail = str_replace(" ", "", $mail);
+                $nAnswer->answer = $mail;
                 $nAnswer->id_event = $nEvent->id;
                 $nAnswer->save();
                 $i++;
